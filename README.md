e4781ce09c07f109c611090a846bedc36a8d7478
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-07-24 12:37:21 -0700

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# ePayments Employee Giving
+
+# Updates 
+
+## Where are the files I need?
+```
+dist/
+├── assets/ (Contains files that come included with MasterPage)
+├── css/
+│   └── custom.css
+└── js/
+│   └── custom.js
+└── index.html
+```
---
+++ src/js/custom.js
@@ -0,0 +1,22 @@
+// Remove search item
+$('.search-item').click(function(){
+  $(this).remove();
+});
+// Remove all search items
+$('#clear-all').click(function(){
+  $('.search-item').remove();
+});
+
+$('.row.row-offcanvas').addClass('onoffcanvas-container');
+
+const $onoffcanvas = $("#onoffcanvas");
+//Close panel on body click
+$('body').click(function(e) {
+  if (!$onoffcanvas.is(e.target) && $onoffcanvas.has(e.target).length === 0 && $onoffcanvas.hasClass('is-open')) {
+    $onoffcanvas.onoffcanvas('hide');  // esc
+  }
+});
+//Close panel on esc key
+$(document).keyup(function(e) {
+  if (e.keyCode === 27 && $onoffcanvas.hasClass('is-open')) $onoffcanvas.onoffcanvas('hide');  // esc
+});
