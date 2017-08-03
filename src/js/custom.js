b0450636261438dd11384426776e28d3de0f8802
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-08-03 09:04:24 -0700

    Fixing bug with modals and offcanvas tab

--- src/js/custom.js
+++ src/js/custom.js
@@ -11,11 +11,11 @@ $('.row.row-offcanvas').addClass('onoffcanvas-container');
 
 const $onoffcanvas = $("#onoffcanvas");
 //Close panel on body click
-$('body').click(function(e) {
-  if (!$onoffcanvas.is(e.target) && $onoffcanvas.has(e.target).length === 0 && $onoffcanvas.hasClass('is-open')) {
-    $onoffcanvas.onoffcanvas('hide');  // esc
-  }
-});
+// $('body').click(function(e) {
+//   if (!$onoffcanvas.is(e.target) && $onoffcanvas.has(e.target).length === 0 && $onoffcanvas.hasClass('is-open')) {
+//     $onoffcanvas.onoffcanvas('hide');  // esc
+//   }
+// });
 //Close panel on esc key
 $(document).keyup(function(e) {
   if (e.keyCode === 27 && $onoffcanvas.hasClass('is-open')) $onoffcanvas.onoffcanvas('hide');  // esc
